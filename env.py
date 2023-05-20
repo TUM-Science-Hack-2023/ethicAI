ee5893cc227428cbe1cbc72e30dda320505dcbe6
Altay <altaykacan@gmail.com>
2023-05-20 21:45:23 +0200

    updating env.py location

---
+++ env.py
@@ -0,0 +1,7 @@
+"""
+This is a python file where you can define your API keys and
+then import into other programs as a module. DON'T PUSH YOUR API KEY
+"""
+
+COHERE_KEY = ""
+OPEN_AI_KEY = "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" # DO NOT COMMIT THIS .gitignore doesn't work
