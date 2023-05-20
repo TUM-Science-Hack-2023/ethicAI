558e4668ad6fcbbca4efde77496ccf7fe42b23cc
philippwulff <philipp.sylt@gmail.com>
2023-05-20 14:09:33 +0200

    minor

--- Home.py
+++ Home.py
@@ -5,15 +5,26 @@ from components.solution_prototype import SolutionPrototype
 
 
 
+# SETUP SESSION STATE
+# Called after a page load/refresh
+
+if "chat_bot" not in st.session_state:
+    st.session_state["chat_bot"] = ChatBot()
+
+# SETUP FOR THIS RENDER
+
+chat_bot = st.session_state["chat_bot"]
+
+# SETUP PAGE INFO
+
 st.set_page_config(
     page_title="EthicAI - Demo",
     page_icon=":robot:"
 )
 
+# RENDER THE PAGE
+
 st.write("# Welcome to EthicAI! 👋")
-
-
-chat_bot = ChatBot()
 
 
 chat_bot.render()
--- components/chat_bot.py
+++ components/chat_bot.py
@@ -2,13 +2,36 @@ import streamlit as st
 from streamlit_chat import message
 
 
+DUMMY_CHAT_HISTORY =  [  
+    {'role':'system', 'content':'You are an assistant that speaks like Shakespeare.'},    
+    {'role':'user', 'content':'tell me a joke'},   
+    {'role':'assistant', 'content':'Why did the chicken cross the road'},   
+    {'role':'user', 'content':'I don\'t know'}  
+]
+
+
 class ChatBot:
     
     def __init__(self) -> None:
         # List of Dicts
-        self.chat_history = []
+        self.chat_history = DUMMY_CHAT_HISTORY
+        
+    def step_chat(self, user_input: str):
+        self.chat_history.append({
+            'role': 'user',
+            'content': user_input
+        })
+        # do API call
     
     def render(self):
         
-        message("My message") 
-        message("Hello bot!", is_user=True)     
+        for msg in self.chat_history:
+            msg_role = msg["role"]
+            msg_content = msg["content"]
+            message(msg_content, is_user=msg_role=="user")
+        
+        # 
+        input_text = st.text_input("You: ", placeholder="Type here..." , key="chat_user_input")
+        
+        if input_text:
+            self.step_chat(input_text)
