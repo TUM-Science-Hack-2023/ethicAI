0ec5e0a419561b9a5dfb1720424e9c01150ec91c
Altay <altaykacan@gmail.com>
2023-05-20 14:20:07 +0200

    Added dummy function

--- components/chat_bot.py
+++ components/chat_bot.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import openai
+import random
 from streamlit_chat import message
 
 
@@ -10,11 +12,46 @@ DUMMY_CHAT_HISTORY =  [
 ]
 
 
-class ChatBot:
-    
+class ChatBot: 
     def __init__(self) -> None:
+        """
+        This is the main class for interacting with the chatbot from the front-end.
+        The chat history is saved as a list of dictionaries, e.g.:
+
+        ```
+        messages =  [  
+        {'role':'system', 'content':'You are an assistant that speaks like a Pirate.'},    
+        {'role':'user', 'content':'tell me a joke'},   
+        {'role':'assistant', 'content':'Why did the chicken cross the road'},   
+        {'role':'user', 'content':'I don\'t know'}  ]
+        ```
+
+        There are three roles for the openAI chatbot API, 'system', 'user', and
+        'assistant'.
+
+        """
+        self.system_prompt = """ """
+        
         # List of Dicts
-        self.chat_history = DUMMY_CHAT_HISTORY
+        self.chat_history = []
+
+        # Init chat history with system prompt
+        self.chat_history.append({"role":"system", "content":self.system_prompt})
+
+    def get_completion_from_history(self, messages, model="gpt-3.5-turbo", temperature=0):
+        response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature, # this is the degree of randomness of the model's output
+    )
+        
+    def get_completion_from_history_dummy(self, message, model=None, temperature=0):
+        answers = ["How does that make you feel?", "Hmmmm", "Can you tell me more about that?", "bruh", "LOL"]
+        response = random.choice(answers)
+
+        return response
+
+
         
     def step_chat(self, user_input: str):
         self.chat_history.append({
