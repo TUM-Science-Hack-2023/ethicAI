5a6be06436174a2eac648715a25856bbbeebf522
Altay <altaykacan@gmail.com>
2023-05-20 21:48:16 +0200

    delete env.py in components

--- components/env.py
+++
@@ -1,7 +0,0 @@
-"""
-This is a python file where you can define your API keys and
-then import into other programs as a module. DON'T PUSH YOUR API KEY
-"""
-
-COHERE_KEY = ""
-OPEN_AI_KEY = "ccccccccccccccc"
