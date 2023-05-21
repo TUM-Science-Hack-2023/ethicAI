a7ad5922c154fbd931e4848552437bef4c798c68
philippwulff <philipp.sylt@gmail.com>
2023-05-21 10:17:34 +0200

    cleanup, storing API keys as st secrets now

--- env.py
+++
@@ -1,8 +0,0 @@
-"""
-This is a python file where you can define your API keys and
-then import into other programs as a module. DON'T PUSH YOUR API KEY
-"""
-
-COHERE_KEY = ""
-OPEN_AI_KEY = "" # DO NOT COMMIT THIS .gitignore doesn't work
-OPEN_AI_KEY2 = "" # Second key to deal with rate limits
